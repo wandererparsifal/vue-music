6d9a5e07a65af70f05c9523aede2f5c3caa5aec6
wandererparsifal <wandererparsifal@gmail.com>
2018-02-01 16:48:28 +0800

    音量图标，重新排列按钮

--- src/components/PlayPanel.vue
+++ src/components/PlayPanel.vue
@@ -3,14 +3,15 @@
     <audio ref="audio" :src="audioPath"
            controls="controls"></audio>
     <div class="panel">
-      <img class="blank" src="">
       <img class="prev" @click="onButtonClick('prev')" src="../assets/icon_prev.svg">
       <img class="play" ref="play" @click="onButtonClick('play')" :src="iconPlay">
       <img class="next" @click="onButtonClick('next')" src="../assets/icon_next.svg">
       <img class="mode" @click="onButtonClick('mode')" src="../assets/icon_shuffle.svg">
+      <img class="volume" @click="onButtonClick('volume')" :src="iconVolume">
+      <i-progress class="progress-volume" :percent="70" hide-info :stroke-width="6"></i-progress>
     </div>
     <div class="panel">
-      <i-progress class="progress" :percent="25" hide-info></i-progress>
+      <i-progress class="progress-music" :percent="25" hide-info></i-progress>
     </div>
   </div>
 </template>
@@ -19,6 +20,8 @@
   import svgStop from '@/assets/icon_stop.svg';
   import svgPause from '@/assets/icon_pause.svg';
   import svgPlay from '@/assets/icon_play.svg';
+  import svgVolume from '@/assets/icon_volume.svg';
+  // import svgMute from '@/assets/icon_mute.svg';
   import EventBus from '../eventBus';
 
   let isPlaying = false;
@@ -31,6 +34,7 @@
       return {
         audioPath: '',
         iconPlay: svgStop,
+        iconVolume: svgVolume,
       };
     },
     methods: {
@@ -74,49 +78,55 @@
 <style scoped>
   .panel {
     width: 100vw;
-    height: 6vh;
+    height: 2vw;
     background-color: rgba(255, 255, 255, 0.5);
     display: flex;
-    justify-content: center;
     align-items: center;
     clear: both;
   }
 
-  .blank {
-    margin-top: 2vh;
-    margin-right: 15vw;
-    width: 5vh;
-    height: 5vh;
-  }
-
   .prev {
     margin-top: 2vh;
-    margin-right: 15vw;
-    width: 5vh;
-    height: 5vh;
+    margin-left: 41vw;
+    width: 2vw;
+    height: 2vw;
   }
 
   .play {
     margin-top: 2vh;
-    width: 5vh;
-    height: 5vh;
+    margin-left: 5vw;
+    width: 2vw;
+    height: 2vw;
   }
 
   .next {
     margin-top: 2vh;
-    margin-left: 15vw;
-    width: 5vh;
-    height: 5vh;
+    margin-left: 5vw;
+    width: 2vw;
+    height: 2vw;
   }
 
   .mode {
     margin-top: 2vh;
     margin-left: 15vw;
-    width: 5vh;
-    height: 5vh;
+    width: 2vw;
+    height: 2vw;
   }
 
-  .progress {
+  .volume {
+    margin-top: 2vh;
+    margin-left: 5vw;
+    width: 2vw;
+    height: 2vw;
+  }
+
+  .progress-volume {
+    margin-top: 2vh;
+    margin-left: 0.5vw;
+    width: 8vw;
+  }
+
+  .progress-music {
     margin-top: 1vh;
     margin-left: -2vw;
     width: 92vw;
