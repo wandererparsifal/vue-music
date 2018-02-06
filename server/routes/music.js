c6780baa7d81b3495da23c9c9ce2b3be203d09b8
wandererparsifal <wandererparsifal@gmail.com>
2018-02-06 14:16:58 +0800

    用Promise重写异步处理

--- server/routes/music.js
+++ server/routes/music.js
@@ -18,47 +18,74 @@ router.get('/path', (req, res, next) => { // eslint-disable-line
   });
 });
 
+function readDirAsync(path) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path, (error, files) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(files);
+      }
+    });
+  });
+}
+
+function statAsync(path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (error, stats) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stats);
+      }
+    });
+  });
+}
+
 router.get('/', (req, res, next) => { // eslint-disable-line
   const array = [];
-  fs.readdir(PATH, (err, files) => {
-    if (err) throw err; // todo 错误跳转
-    const length = files.length;
-    let count = 0; // todo 循环异步解决方案
-    files.forEach((fileName) => {
-      const musicDir = path.join(PATH, fileName);
-      fs.stat(musicDir, (err, stats) => {
-        if (err) throw err; // todo 错误跳转
-        if (stats.isDirectory()) {
-          fs.readdir(musicDir, (err, musics) => {
-            if (err) throw err;
-            const musicList = [];
-            musics.forEach((musicName) => {
-              musicList.push({
+  const musicDirs = [];
+  readDirAsync(PATH)
+    .then((files) => { // eslint-disable-line
+      return Promise.all(files.map((file) => {
+        const musicDir = path.join(PATH, file);
+        return statAsync(musicDir)
+          .then((stat) => {
+            if (stat.isDirectory()) {
+              musicDirs.push(musicDir);
+            }
+          });
+      }));
+    })
+    .then(() => {
+      console.log('musicDirs', musicDirs);
+      return Promise.all(musicDirs.map((musicDir) => { // eslint-disable-line
+        return readDirAsync(musicDir)
+          .then((musicArray) => {
+            console.log('musicArray', musicArray);
+            const musicList = musicArray.map((musicName) => { // eslint-disable-line
+              return {
                 title: path.basename(musicName, '.mp3'),
                 path: path.join(musicDir, musicName),
                 artist: 'artist',
                 album: 'album',
                 id: uuidv4(),
-              });
+              };
             });
             array.push({
               name: (array.length + 1).toString(),
-              dirName: fileName,
+              dirName: path.basename(musicDir),
               list: musicList,
               index: array.length,
             });
-            count += 1;
-            if (count === length) {
-              console.log('array', JSON.stringify(array));
-              res.json(array);
-            }
           });
-        } else {
-          count += 1;
-        }
-      });
-    });
-  });
+      }));
+    })
+    .then(() => {
+      console.log('array', JSON.stringify(array));
+      res.json(array);
+    })
+    .catch(error => console.log(error));
 });
 
 module.exports = router;
