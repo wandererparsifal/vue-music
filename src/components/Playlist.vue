5fccf2cd473dcdcae9d5057902b2eeec27c90aab
wandererparsifal <wandererparsifal@gmail.com>
2018-02-07 14:56:13 +0800

    重写播放列表组件

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,68 +1,46 @@
 <template>
-  <i-table ref="refPlaylist" class="table" :height="height" :highlight-row="true" stripe :columns="columns" :data="data"
-           @on-row-click="rowClicked"/>
+  <div class="list-wrapper">
+    <div class="list-item" v-for="(music, index) in data" :key="data.id"
+         :style="{backgroundColor: highlightRow === index ? '#7be6ff' : (index % 2 === 0 ? '#e0ffff' : '#FFFDFD')}"
+         @click="rowClicked(music, index)">
+      <div class="title-wrapper">
+        <div class="music-text">
+          {{music.title}}
+        </div>
+      </div>
+      <div class="artist-wrapper">
+        <div class="music-text">
+          {{music.artist}}
+        </div>
+      </div>
+      <div class="album-wrapper">
+        <div class="music-text">
+          {{music.album}}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   import EventBus from '../eventBus';
 
-  let tablePlaylist = null;
-
   export default {
     name: 'Playlist',
     data() {
       return {
-        height: 0,
-        columns: [
-          {
-            title: 'Title',
-            key: 'title',
-            align: 'left',
-          },
-          {
-            title: 'Artist',
-            key: 'artist',
-            align: 'center',
-          },
-          {
-            title: 'Album',
-            key: 'album',
-            align: 'center',
-          },
-          {
-            title: ' ',
-            key: 'xxx',
-            align: 'center',
-            render: (h, params) => { // eslint-disable-line
-              return h('div', [
-                h('Icon', {
-                  props: {
-                    type: 'shuffle',
-                  },
-                  nativeOn: { // Icon 需要用 nativeOn，button 用 on
-                    click: () => {
-                      console.log('click Icon');
-                    },
-                  },
-                }, 'View'),
-              ]);
-            },
-          },
-        ],
         data: [],
         musicIds: [],
+        highlightRow: -1,
       };
     },
     methods: {
       rowClicked(musicData, index) {
+        this.highlightRow = index;
         EventBus.$emit('EVENT_MUSIC_PLAY', musicData, index);
       },
     },
     created() {
-      this.height = document.documentElement.clientHeight * 0.79;
-      window.addEventListener('resize', () => {
-        this.height = document.documentElement.clientHeight * 0.79;
-      });
       EventBus.$on('EVENT_MUSIC_ADDED', (musicData) => {
         if (!this.musicIds.includes(musicData.id)) {
           this.musicIds.push(musicData.id);
@@ -70,17 +48,52 @@
         }
       });
       EventBus.$on('EVENT_HIGHLIGHT_ROW', (index) => {
-        tablePlaylist.highlightCurrentRow(index);
+        console.log('highlightRow', this.highlightRow);
+        this.highlightRow = index;
       });
     },
     mounted() {
-      tablePlaylist = this.$refs.refPlaylist;
     },
   };
 </script>
 
 <style>
-  .table {
-    opacity: 1;
+  .list-wrapper {
+    overflow-y: auto;
+    max-height: 79vh;
+    height: 79vh;
+    background-color: #F8F8FF;
+  }
+
+  .list-item {
+    display: flex;
+    align-items: center;
+    height: 6vh;
+    font-size: 1rem;
+    color: #00477D;
+  }
+
+  .title-wrapper {
+    display: flex;
+    margin-left: 2vw;
+    width: 23vw;
+  }
+
+  .artist-wrapper {
+    display: flex;
+    margin-left: 5vw;
+    width: 20vw;
+  }
+
+  .album-wrapper {
+    display: flex;
+    margin-left: 5vw;
+    width: 20vw;
+  }
+
+  .music-text {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
   }
 </style>
